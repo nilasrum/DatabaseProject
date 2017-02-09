7f0761ebf6e648dd97fa2cf4de011911a58b6766
nilasrum <tanveer.2387@gmail.com>
2017-02-09 20:29:55 +0600

    upcoming details modal window added,read more funtionality added.gallery photo resized,hover effect added.

--- home/templatetags/fiter_tag_home.py
+++ home/templatetags/fiter_tag_home.py
@@ -8,3 +8,11 @@ register = template.Library()
 def input_type(input):
     return input.field.widget.__class__.__name__
 
+@register.filter('str_cut')
+def str_cut(input):
+    l = len(input)
+    if (l>250):
+        s=input[:250]+'...'
+        return s
+    else:
+        return input
